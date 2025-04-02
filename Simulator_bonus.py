6614ed369811e89ea4dc51843692abd8466346fb
YadavShabd02 <shabd24516@iiitd.ac.in>
2025-04-02 17:54:12 +0530

    Final Prject with bonus

--- Simulator_bonus.py
+++ Simulator_bonus.py
@@ -23,7 +23,7 @@ def bin_to_dec(binary, signed=True):
     if not signed:
         return int(binary, 2)
     if binary[0] == '1':
-        return int(binary, 2) - (2 ** len(binary))
+        return int(binary, 2) - (1 << len(binary))
     return int(binary, 2)
 
 def dec_to_bin(val, bits=32):
@@ -34,11 +34,11 @@ def dec_to_bin(val, bits=32):
 def extract_fields(binary):
     fields = {
         'opcode': binary[25:],            # bits [6:0]
-        'rd': int(binary[20:25], 2),      # bits [11:7]
-        'funct3': binary[17:20],          # bits [14:12]
-        'rs1': int(binary[12:17], 2),     # bits [19:15]
-        'rs2': int(binary[7:12], 2),      # bits [24:20]
-        'funct7': binary[0:7],            # bits [31:25]
+        'rd': int(binary[20:25], 2),        # bits [11:7]
+        'funct3': binary[17:20],           # bits [14:12]
+        'rs1': int(binary[12:17], 2),       # bits [19:15]
+        'rs2': int(binary[7:12], 2),        # bits [24:20]
+        'funct7': binary[0:7],             # bits [31:25]
         'imm_i': bin_to_dec(binary[0:12]),
         'imm_b': bin_to_dec(binary[0] + binary[24] + binary[1:7] + binary[20:24] + '0', signed=True),
         'imm_s': bin_to_dec(binary[0:7] + binary[20:25], signed=True),
@@ -63,7 +63,26 @@ def handle_r_type(fields):
     elif funct3 == '111':  # and
         result = REG[rs1] & REG[rs2]
     if rd != 0:
-        REG[rd] = result
+        REG[rd] = result & 0xFFFFFFFF
+
+def handle_special_instructions(fields):
+    """
+    Handle bonus instructions that are not standard.
+    In this version we only implement:
+      - rst: reset registers (opcode '1110011', funct3 '000', funct7 '0000000')
+      - halt: stop simulation (opcode '1110011', funct3 '001', funct7 '0000000')
+    """
+    global PC
+    opcode = fields['opcode']
+    funct3 = fields['funct3']
+    funct7 = fields['funct7']
+    if opcode == '1110011' and funct3 == '000' and funct7 == '0000000':
+        # Reset all registers except x0 (which is always 0)
+        for i in range(1, 32):
+            REG[i] = 0
+    elif opcode == '1110011' and funct3 == '001' and funct7 == '0000000':
+        # Halt execution by setting PC high enough to break the simulation loop
+        PC = 999999999
 
 def handle_i_type(fields):
     global PC
@@ -71,18 +90,18 @@ def handle_i_type(fields):
     imm = fields['imm_i']
     if fields['opcode'] == '0010011':  # addi
         if rd != 0:
-            REG[rd] = REG[rs1] + imm
+            REG[rd] = (REG[rs1] + imm) & 0xFFFFFFFF
     elif fields['opcode'] == '1100111':  # jalr
         temp = PC + 4
         PC = (REG[rs1] + imm) & ~1
         if rd != 0:
-            REG[rd] = temp
+            REG[rd] = temp & 0xFFFFFFFF
     elif fields['opcode'] == '0000011':  # lw
         address = REG[rs1] + imm
         key = f"0x{address:08x}"
         val = memory_values.get(key, 0)
         if rd != 0:
-            REG[rd] = val
+            REG[rd] = val & 0xFFFFFFFF
 
 def handle_s_type(fields):
     rs1, rs2 = fields['rs1'], fields['rs2']
@@ -97,7 +116,7 @@ def handle_j_type(fields):
     link_val = PC + 4
     PC = PC + fields['imm_j']
     if rd != 0:
-        REG[rd] = link_val
+        REG[rd] = link_val & 0xFFFFFFFF
 
 def handle_b_type(fields):
     rs1, rs2 = fields['rs1'], fields['rs2']
@@ -110,20 +129,24 @@ def handle_b_type(fields):
 
 def handle_instruction(fields):
     opcode = fields['opcode']
-    if opcode == '0110011':
+    if opcode == '0110011':       # R-type
         handle_r_type(fields)
-    elif opcode in ['0010011', '1100111', '0000011']:
+    elif opcode in ['0010011', '1100111', '0000011']:  # I-type
         handle_i_type(fields)
-    elif opcode == '0100011':
+    elif opcode == '0100011':     # S-type
         handle_s_type(fields)
-    elif opcode == '1101111':
+    elif opcode == '1101111':     # J-type
         handle_j_type(fields)
+    else:
+        # Check for our custom bonus instructions (rst/halt)
+        handle_special_instructions(fields)
 
 def dump_state(fout):
     fout.write(f"{PC} " + " ".join(str(REG[i] & 0xFFFFFFFF) for i in range(32)) + "\n")
 
 def dump_state_binary(fout):
-        fout.write(f"0b{dec_to_bin(PC)}" + " " + " ".join(f"0b{dec_to_bin(REG[i] & 0xFFFFFFFF)}" for i in range(32)) + "\n")
+    fout.write("0b" + dec_to_bin(PC) + " " +
+               " ".join("0b" + dec_to_bin(REG[i] & 0xFFFFFFFF) for i in range(32)) + "\n")
 
 def dump_memory(fout):
     for key in ORIG_MEMORY_KEYS:
@@ -131,7 +154,7 @@ def dump_memory(fout):
 
 def dump_memory_binary(fout):
     for key in ORIG_MEMORY_KEYS:
-        fout.write(f"{key}:0b{dec_to_bin(memory_values[key])}\n")
+        fout.write(f"{key}:0b" + dec_to_bin(memory_values[key]) + "\n")
 
 def simulate(input_file, output_file_decimal, output_file_binary):
     global PC, REG, memory_values
@@ -146,34 +169,28 @@ def simulate(input_file, output_file_decimal, output_file_binary):
     with open(output_file_decimal, 'w') as fout_dec, open(output_file_binary, 'w') as fout_bin:
         while PC < len(instructions) * 4:
             instr = instructions[PC // 4]
-            if instr == '00000000000000000000000001100011':  # HALT
+            # Check for the existing HALT instruction (if present)
+            if instr == '00000000000000000000000001100011':
                 dump_state(fout_dec)
                 dump_state_binary(fout_bin)
                 break
-            if instr=='00000000000000000000000000000000':
-                REG=[0]*32
-                dump_state(fout_dec)
-                dump_state_binary(fout_bin)
-                PC += 4
-                continue
 
             fields = extract_fields(instr)
-            if fields['opcode'] == '1100011':  # B-type
+            if fields['opcode'] == '1100011':  # B-type branch
                 offset = handle_b_type(fields)
                 if offset == 0:
                     PC += 4
                 else:
                     PC += offset
-                if instructions[PC // 4] == '00000000000000000000000001100011':
+                if PC // 4 < len(instructions) and instructions[PC // 4] == '00000000000000000000000001100011':
                     dump_state(fout_dec)
                     dump_state_binary(fout_bin)
                     break
-                else:
-                    dump_state(fout_dec)
-                    dump_state_binary(fout_bin)
+                dump_state(fout_dec)
+                dump_state_binary(fout_bin)
             else:
                 handle_instruction(fields)
-                if fields['opcode'] not in ['1100111', '1101111']:  # not jalr/jal
+                if fields['opcode'] not in ['1100111', '1101111']:
                     PC += 4
                 dump_state(fout_dec)
                 dump_state_binary(fout_bin)
